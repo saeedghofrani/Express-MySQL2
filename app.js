fab5732f65d3f57ce091a03866ea277c6a4bc031
Saeed <sa.ghofraniivari@gmail.com>
2022-01-07 20:11:54 +0330

    base

---
+++ app.js
@@ -0,0 +1,28 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const morgan = require('morgan');
+const app = express();
+app.use(morgan('tiny'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// catch 404 and forward to error handler
+app.use(function (_req, _res, next) { next(createError(404)); });
+// error handler
+app.use(function (err, req, res, _next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  req.accepts('html') ? res.render('error') : req.accepts('json') ? res.send({ error: 'Not found' }) : res.type('txt').send('Not found');
+});
+module.exports = app;
